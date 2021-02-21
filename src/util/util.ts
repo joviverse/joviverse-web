c1c03ff45c2b315a3748fc5df9615e6de2e74ac9
Sunghyun Cho <mail@chosunghyun.com>
2021-02-21 23:33:09 +0900

    Add cookie & localStorage handlers

--- src/util/util.ts
+++ src/util/util.ts
@@ -1,3 +1,26 @@
-export const saveJwtToLocalStorage = (token: string) => {
-  localStorage.setItem('token', token)
+export const parseCookieToJson = (cookie: string) => {
+  return Object.fromEntries(cookie.split('; ').map((x) => x.split('=')))
+}
+
+export const clearCookie = () => {
+  document.cookie = `token=;expires=Thu, 01 Jan 1970 00:00:00 GMT;path=/`
+}
+
+export const saveJsonToLocalStorage = (jsonObj: any) => {
+  Object.keys(jsonObj).map((key) => {
+    localStorage.setItem(key, jsonObj[key])
+  })
+}
+
+export const getLocalJwt = () => {
+  if (typeof window !== 'undefined') {
+    console.log(localStorage['token'])
+    return localStorage['token']
+  } else return 'undefined'
+}
+
+export const setLocalJwt = (token: string) => {
+  if (typeof window !== 'undefined') {
+    localStorage['token'] = token
+  }
 }
