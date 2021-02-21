e6f902134beb6d1d2504e426f95e9e5a3b296dd6
Sunghyun Cho <mail@chosunghyun.com>
2021-02-21 23:32:55 +0900

    Add Types to useInput Custom Hook

--- src/hooks/useInput.ts
+++ src/hooks/useInput.ts
@@ -1,14 +1,12 @@
-import React, { useState, useCallback, ChangeEvent } from 'react'
+import { useState, useCallback, ChangeEvent } from 'react'
 
 const useInput = (initialValue: string) => {
   const [value, setValue] = useState(initialValue)
-  const handler: (event: ChangeEvent<HTMLInputElement>) => void = useCallback(
-    (e) => {
-      setValue(e.target.value)
-    },
-    []
-  )
+  const handler:
+    | ((event: ChangeEvent<HTMLInputElement>) => void)
+    | undefined = useCallback((e) => {
+    setValue(e.target.value)
+  }, [])
   return [value, handler]
 }
-
 export default useInput
